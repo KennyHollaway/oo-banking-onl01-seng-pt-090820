efbca60eec01af09a4acd3d322e8858b0d7be108
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-07 23:18:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,15 +17,15 @@ class Transfer
   
   def execute_transaction
     if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+       @status = "rejected"
+       return "Transaction rejected. Please check your account balance."
 
     elsif @status == "complete"
-      puts "Transaction was already excuted"
+       puts "Transaction was already excuted"
     else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
+       @sender.deposit( @amount * -1 ) 
+       @receiver.deposit( @amount )
+       @status = "complete"
     end
   end
 end
