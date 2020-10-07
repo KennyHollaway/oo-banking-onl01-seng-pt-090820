8eca9cf697fcabe46817e48b780400b9c7cb132f
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-07 23:12:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,5 +2,7 @@ class Transfer
   attr_accessor :sender, :reciever, :amount, :status
   def initialize(sender, reciever, amount)
     @sender = sender
+    @reciever = reciever
+    @amount = amount
   end
 end
