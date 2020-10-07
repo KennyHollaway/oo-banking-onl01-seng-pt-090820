c0550bc80aebd41104712efdae4746014cd89f82
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-07 23:26:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,5 +15,14 @@ class Transfer
     end
   end
   
-  
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
 end
